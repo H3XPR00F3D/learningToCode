900883942d713ea60c3a4a640e9e429ef31f35cb
H3XPR00F3D <david@ddornart.com>
2022-12-19 14:33:53 -0500

    Ok, it's pushing and pulling and committing now. all is well.

--- HelloWorldTut/Tutorials3.cs
+++ HelloWorldTut/Tutorials3.cs
@@ -3,20 +3,19 @@
 */
 
 using System;
-
 namespace Tutorialling
 {
-
     internal class Program
     {
         private static void Main(string[] args)
         {
-
-
-
             Console.WriteLine();
 
-            Console.BackgroundColor = ConsoleColor.Green;
+
+
+
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor= ConsoleColor.Green;
             Console.WriteLine("");
             Console.WriteLine("Press any key to close.");
             Console.ReadLine();
