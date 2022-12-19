3faeb55e11a7e3f62ce7db45f7a74dd5a606787f
H3XPR00F3D <david@ddornart.com>
2022-12-19 14:05:10 -0500

    hello

--- HelloWorldTut/Tutorials3.cs
+++ HelloWorldTut/Tutorials3.cs
@@ -11,7 +11,7 @@ namespace Tutorialling
         {
             Console.WriteLine();
 
-
+            Console.BackgroundColor = ConsoleColor.Green;
             Console.WriteLine("");
             Console.WriteLine("Press any key to close.");
             Console.ReadLine();
